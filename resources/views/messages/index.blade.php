e06b2897d5308610c09fb3b4cf51a8121993386f
yuu.nakata <tanaka_yi7@yahoo.co.jp>
2020-05-21 12:48:09 +0000

    Procfile

--- resources/views/messages/index.blade.php
+++ resources/views/messages/index.blade.php
@@ -25,5 +25,11 @@
             </tbody>
         </table>
     @endif
+    
+    {{-- ページネーションのリンク --}}
+    {{ $messages->links() }}
+
+    {{-- メッセージ作成ページへのリンク --}}
+    {!! link_to_route('messages.create', '新規メッセージの投稿', [], ['class' => 'btn btn-primary']) !!}
 
 @endsection
